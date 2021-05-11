7e1396df3d9127f30664074e52e8875fd39e5cf1
Vlastimil Babka <vbabka@suse.cz>
2021-05-11 16:37:51 +0200

    mm, slub: validate partial and newly allocated slabs before loading them
    When we obtain a new slab page from node partial list or page allocator, we
    assign it to kmem_cache_cpu, perform some checks, and if they fail, we undo
    the assignment.
    
    In order to allow doing the checks without irq disabled, restructure the code
    so that checks go first, and kmem_cache_cpu assignment only afterwards.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- mm/slub.c
+++ mm/slub.c
@@ -2743,10 +2743,8 @@ new_slab:
 new_objects:
 
 	freelist = get_partial(s, gfpflags, node, &page);
-	if (freelist) {
-		c->page = page;
+	if (freelist)
 		goto check_new_page;
-	}
 
 	local_irq_restore(flags);
 	migrate_enable();
@@ -2760,9 +2758,6 @@ new_objects:
 	}
 
 	local_irq_save(flags);
-	if (c->page)
-		flush_slab(s, c);
-
 	/*
 	 * No other reference to the page yet so we can
 	 * muck around with it freely without cmpxchg
@@ -2771,14 +2766,12 @@ new_objects:
 	page->freelist = NULL;
 
 	stat(s, ALLOC_SLAB);
-	c->page = page;
 
 check_new_page:
 
 	if (kmem_cache_debug(s)) {
 		if (!alloc_debug_processing(s, page, freelist, addr)) {
 			/* Slab failed checks. Next slab needed */
-			c->page = NULL;
 			local_irq_restore(flags);
 			goto new_slab;
 		} else {
@@ -2797,9 +2790,17 @@ check_new_page:
 		 */
 		goto return_single;
 
+	if (unlikely(c->page))
+		flush_slab(s, c);
+	c->page = page;
+
 	goto load_freelist;
 
 return_single:
+
+	if (unlikely(c->page))
+		flush_slab(s, c);
+	c->page = page;
 
 	deactivate_slab(s, page, get_freepointer(s, freelist), c);
 	local_irq_restore(flags);
