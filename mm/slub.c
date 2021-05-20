7d11f8bfd41c514b5ae1a127c51a8e76a6841305
Vlastimil Babka <vbabka@suse.cz>
2021-05-21 01:16:54 +0200

    mm, slub: only disable irq with spin_lock in __unfreeze_partials()
    __unfreeze_partials() no longer needs to have irqs disabled, except for making
    the spin_lock operations irq-safe, so convert the spin_locks operations and
    remove the separate irq handling.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- mm/slub.c
+++ mm/slub.c
@@ -2301,8 +2301,6 @@ static void __unfreeze_partials(struct kmem_cache *s, struct page *partial_page)
 	struct page *page, *discard_page = NULL;
 	unsigned long flags;
 
-	local_irq_save(flags);
-
 	while (partial_page) {
 		struct page new;
 		struct page old;
@@ -2313,10 +2311,10 @@ static void __unfreeze_partials(struct kmem_cache *s, struct page *partial_page)
 		n2 = get_node(s, page_to_nid(page));
 		if (n != n2) {
 			if (n)
-				spin_unlock(&n->list_lock);
+				spin_unlock_irqrestore(&n->list_lock, flags);
 
 			n = n2;
-			spin_lock(&n->list_lock);
+			spin_lock_irqsave(&n->list_lock, flags);
 		}
 
 		do {
@@ -2345,9 +2343,7 @@ static void __unfreeze_partials(struct kmem_cache *s, struct page *partial_page)
 	}
 
 	if (n)
-		spin_unlock(&n->list_lock);
-
-	local_irq_restore(flags);
+		spin_unlock_irqrestore(&n->list_lock, flags);
 
 	while (discard_page) {
 		page = discard_page;
