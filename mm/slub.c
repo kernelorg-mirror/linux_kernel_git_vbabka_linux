b0be1e461c85efad236409bfab88b27f2ca7c978
Vlastimil Babka <vbabka@suse.cz>
2021-05-21 14:03:23 +0200

    mm, slub: use migrate_disable() in put_cpu_partial()
    In put_cpu_partial, we need a stable cpu, but being preempted is not an issue.
    So, disable migration instead of preemption.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>

--- mm/slub.c
+++ mm/slub.c
@@ -2417,7 +2417,7 @@ static void put_cpu_partial(struct kmem_cache *s, struct page *page, int drain)
 	int pages;
 	int pobjects;
 
-	preempt_disable();
+	migrate_disable();
 	do {
 		pages = 0;
 		pobjects = 0;
@@ -2451,7 +2451,7 @@ static void put_cpu_partial(struct kmem_cache *s, struct page *page, int drain)
 	if (unlikely(!slub_cpu_partial(s)))
 		unfreeze_partials(s);
 
-	preempt_enable();
+	migrate_enable();
 #endif	/* CONFIG_SLUB_CPU_PARTIAL */
 }
 
