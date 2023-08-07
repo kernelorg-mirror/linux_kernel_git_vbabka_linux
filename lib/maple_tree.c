f1bef5221fd0cef121d96c20edfbf9699e19c9e5
Vlastimil Babka <vbabka@suse.cz>
2023-08-07 22:43:15 +0200

    maple_tree: avoid bulk alloc/free to use percpu array more
    Using bulk alloc/free on a cache with percpu array should not be
    necessary and the bulk alloc actually bypasses the array (the prefill
    functionality currently relies on this).
    
    The simplest change for now is just to convert the respective maple tree
    wrappers to do a loop of normal alloc/free. We can optimize later
    versions if needed.

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -161,12 +161,20 @@ static inline struct maple_node *mt_alloc_one(gfp_t gfp)
 
 static inline int mt_alloc_bulk(gfp_t gfp, size_t size, void **nodes)
 {
-	return kmem_cache_alloc_bulk(maple_node_cache, gfp, size, nodes);
+	int i;
+
+	for (i = 0; i < size; i++) {
+		nodes[i] = kmem_cache_alloc(maple_node_cache, gfp);
+		if (!nodes[i])
+			break;
+	}
+	return i;
 }
 
 static inline void mt_free_bulk(size_t size, void __rcu **nodes)
 {
-	kmem_cache_free_bulk(maple_node_cache, size, (void **)nodes);
+	for (size_t i = 0; i < size; i++)
+		kmem_cache_free(maple_node_cache, nodes[i]);
 }
 
 static void mt_free_rcu(struct rcu_head *head)
