19c465cd976d9585b93d6b6ea5056e1d4cca878c
Liam R. Howlett <Liam.Howlett@Oracle.com>
2024-12-02 16:33:10 -0500

    tools: Add testing support for changes to rcu and slab for sheaves
    Make testing work for the slab and rcu changes that have come in with
    the sheaves work.
    
    This only works with one kmem_cache, and only the first one used.
    Subsequent setting of keme_cache will not update the active kmem_cache
    and will be silently dropped because there are other tests which happen
    after the kmem_cache of interest is set.
    
    The saved active kmem_cache is used in the rcu callback, which passes
    the object to be freed.
    
    The rcu call takes the rcu_head, which is passed in as the field in the
    struct (in this case rcu in the maple tree node), which is calculated by
    pointer math.  The offset of which is saved (in a global variable) for
    restoring the node pointer on the callback after the rcu grace period
    expires.
    
    Don't use any of this outside of testing, please.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>

--- tools/testing/shared/linux/rcupdate.h
+++ tools/testing/shared/linux/rcupdate.h
@@ -9,4 +9,26 @@
 #define rcu_dereference_check(p, cond) rcu_dereference(p)
 #define RCU_INIT_POINTER(p, v)	do { (p) = (v); } while (0)
 
+void kmem_cache_free_active(void *objp);
+static unsigned long kfree_cb_offset = 0;
+
+static inline void kfree_rcu_cb(struct rcu_head *head)
+{
+	void *objp = (void *) ((unsigned long)head - kfree_cb_offset);
+
+	kmem_cache_free_active(objp);
+}
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER)	__builtin_offsetof(TYPE, MEMBER)
+#endif
+
+#define kfree_rcu(ptr, rhv)						\
+do {									\
+	if (!kfree_cb_offset)						\
+		kfree_cb_offset = offsetof(typeof(*(ptr)), rhv);	\
+									\
+	call_rcu(&ptr->rhv, kfree_rcu_cb);				\
+} while (0)
+
 #endif
