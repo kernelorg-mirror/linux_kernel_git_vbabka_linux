08902aaadd6309c16c77140f7dc6c1948e32a91d
Vlastimil Babka (SUSE) <vbabka@kernel.org>
2026-07-08 17:04:36 +0200

    mm/slab: reduce slabobj_ext memory usage witout allocation profiling
    When memory allocation profiling is compiled in but permanently disabled
    on boot with (implicit or explicit) "never" parameter, stop allocating
    (wasting) memory for the codetag_ref parts of slabobj_ext metadata.
    
    Signed-off-by: Vlastimil Babka (SUSE) <vbabka@kernel.org>

--- mm/slab.h
+++ mm/slab.h
@@ -587,7 +587,7 @@ static inline size_t static_obj_ext_size(void)
 	if (IS_ENABLED(CONFIG_MEMCG))
 		sz += 1;
 
-	if (IS_ENABLED(CONFIG_MEM_ALLOC_PROFILING))
+	if (slab_obj_ext_has_codetag())
 		sz += 1;
 
 	return sizeof(struct slabobj_ext) * sz;
