b596dd8a3ed91b23f0d0fda62939ada9d2ae5b94
Vlastimil Babka (SUSE) <vbabka@kernel.org>
2026-07-10 18:27:32 +0200

    mm/slab: abstract slabobj_ext.ref access
    In preparation for changes to the structure, abstract access to the ref
    field with a slab_obj_ext_codetag_ref() function. Rename the field to
    _ctref to make an unexpected direct access a compile error.
    
    No functional change intended.
    
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Signed-off-by: Vlastimil Babka (SUSE) <vbabka@kernel.org>

--- mm/slab.h
+++ mm/slab.h
@@ -564,7 +564,7 @@ struct slabobj_ext {
 	struct obj_cgroup *_objcg;
 #endif
 #ifdef CONFIG_MEM_ALLOC_PROFILING
-	union codetag_ref ref;
+	union codetag_ref _ctref;
 #endif
 } __aligned(8);
 
@@ -678,6 +678,14 @@ static inline void slab_obj_ext_set_objcg(struct slabobj_ext *obj_ext,
 					  struct obj_cgroup *objcg)
 {
 	obj_ext->_objcg = objcg;
+}
+#endif
+
+#ifdef CONFIG_MEM_ALLOC_PROFILING
+static inline union codetag_ref *
+slab_obj_ext_codetag_ref(struct slab *slab, struct slabobj_ext *obj_ext)
+{
+	return &obj_ext->_ctref;
 }
 #endif
 
